eb571d4623172fc563f10487376acb05bc8432c0
EduardoLz <ejlz95@hotmail.com>
2021-03-14 15:03:37 -0600

    'first'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Shoppingcart />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Shoppingcart from "@/components/Shoppingcart.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    Shoppingcart,
+  },
+};
 </script>
